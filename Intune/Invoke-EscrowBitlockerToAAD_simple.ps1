1e7816a8b0c502e945179205ee324935adae4148
Arran <Arran@deskinthe.cloud>
2022-10-14 14:24:30 +0100

    remediate and bitlocker

---
+++ Intune/Invoke-EscrowBitlockerToAAD_simple.ps1
@@ -0,0 +1,17 @@
+﻿try{
+    $BitlockerVol = Get-BitLockerVolume -MountPoint $env:SystemDrive
+    $KPID=""
+    foreach($KP in $BitlockerVol.KeyProtector){
+        if($KP.KeyProtectorType -eq "RecoveryPassword"){
+            $KPID=$KP.KeyProtectorId
+            break;
+            }
+        }
+        $output = BackupToAAD-BitLockerKeyProtector -MountPoint "$($env:SystemDrive)" -KeyProtectorId $KPID
+        return $true
+    }
+    catch{
+            return $false
+    }
+
+# Try https://call4cloud.nl/2021/02/b-for-bitlocker/
